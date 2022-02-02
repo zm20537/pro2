8a27a91a19e5e50453c79ed2ce52c25698c50832
zm20537 <zm20537@josai.ac.jp>
2022-02-02 08:21:02 +0000

    2.2

--- leftright.js
+++ leftright.js
@@ -1,4 +1,4 @@
-function introimg()
+function introimgn()
 {
     if(document.getElementById('imagec').getAttribute('src') == './intro/bankin.jpg')
     {
@@ -21,6 +21,25 @@ function introimg()
         document.getElementById('imagec').src='./intro/bankin.jpg';
     }
 }
-function asd(){
-    alert ('test');
+function introimgb(){
+    if(document.getElementById('imagec').getAttribute('src') == './intro/bankin.jpg')
+    {
+        document.getElementById('imagec').src='./intro/ohuro.png';
+    }
+    else if(document.getElementById('imagec').getAttribute('src') == './intro/ohuro.png')
+    {
+        document.getElementById('imagec').src='./intro/nobose.png';
+    }
+    else if(document.getElementById('imagec').getAttribute('src') == './intro/nobose.png')
+    {
+        document.getElementById('imagec').src='./intro/eating-snow.png';
+    }
+    else if(document.getElementById('imagec').getAttribute('src') == './intro/eating-snow.png')
+    {
+        document.getElementById('imagec').src='./intro/calorie.jpg';
+    }
+    else
+    {
+        document.getElementById('imagec').src='./intro/bankin.jpg';
+    }
 }
