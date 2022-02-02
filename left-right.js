ab297c6148754c62e16fbca4b0996c1ce4649f48
zm20537 <zm20537@josai.ac.jp>
2022-02-02 07:15:30 +0000

    2.2

--- left-right.js
+++ left-right.js
@@ -1,10 +1,22 @@
-function intro-img(){
-    if(document.getelementbyid('imagec').value== 'src="./image/intro-img1.jpg"')
+function introimg(){
+    if(document.getelementbyid('imagec').value== 'src="./intro/bankin.jpg"')
     {
-        document.getElementById('imagec').src='./image/intro-img2.jpg';
+        document.getElementById('imagec').src='./intro/calorie.jpg';
+    }
+    if else(document.getelementbyid('imagec').value== 'src="./intro/calorie.png"')
+    {
+        document.getElementById('imagec').src='./intro/eating-snow.png';
+    }
+    if else(document.getelementbyid('imagec').value== 'src="./intro/eating-snow.png"')
+    {
+        document.getElementById('imagec').src='./intro/nobose.png';
+    }
+    if else(document.getelementbyid('imagec').value== 'src="./intro/nobose.png"')
+    {
+        document.getElementById('imagec').src='./intro/ohuro.png';
     }
     else
     {
-        alert('はずれ！');
+        document.getElementById('imagec').src='./intro/bankin.jpg';
     }
 }
