b85c46ef4e9fea5d05f35209c9aeb9b463ca3bf1
zm20537 <zm20537@josai.ac.jp>
2022-02-03 02:39:25 +0000

    2.3

--- leftright.js
+++ leftright.js
@@ -43,3 +43,26 @@ function introimgb(){
         document.getElementById('imagec').src='./intro/bankin.jpg';
     }
 }
+function expc()
+{
+    if(document.getElementById('imagec').getAttribute('src') == './intro/bankin.jpg')
+    {
+        document.getElementById('setsumei').innerHTML ='1';
+    }
+    else if(document.getElementById('imagec').getAttribute('src') == './intro/calorie.jpg')
+    {
+        document.getElementById('setsumei').innerHTML ='2';
+    }
+    else if(document.getElementById('imagec').getAttribute('src') == './intro/eating-snow.png')
+    {
+        document.getElementById('setsumei').innerHTML ='3';
+    }
+    else if(document.getElementById('imagec').getAttribute('src') == './intro/nobose.png')
+    {
+        document.getElementById('setsumei').innerHTML ='4';
+    }
+    else
+    {
+        document.getElementById('setsumei').innerHTML ='5';
+    }
+}
