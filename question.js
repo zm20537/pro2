8720c0c2a977ef339636dbfdec7b0a6d861d621e
zm20537 <zm20537@josai.ac.jp>
2022-02-06 07:54:18 +0000

    2.6

--- question.js
+++ question.js
@@ -6,5 +6,5 @@ function toi1f(){
 }
 
 function Commentary1(){
-    document.getElementById('marubatsu').innerHTML('リフレッシュレートを高く出せるモニターだったとしても、CPU、GPU、メモリの性能が足りなければ、意味がありません。そのため、丁度いいリフレッシュレートが出せるモニターを選ぶべきです。')
+    document.getElementById('marubatsu').innerHTML='リフレッシュレートを高く出せるモニターだったとしても、CPU、GPU、メモリの性能が足りなければ、意味がありません。そのため、丁度いいリフレッシュレートが出せるモニターを選ぶべきです。';
 }
